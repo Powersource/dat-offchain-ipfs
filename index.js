ce4e0ae3dbcf550a8dd0992556d3adb8b9da5dfc
Jacob Karlsson <jacob.karlsson95@gmail.com>
2019-05-09 22:56:21 +0200

    Add dat

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
-const ipfsConstructor = require('ipfs')
+const IPFS = require('ipfs')
+const Dat = require('dat')
 
-const ipfs = new ipfsConstructor()
+//exports.init = functi
+const ipfs = new IPFS()
 
 ipfs.on('ready', async () => {
     console.log('ipfs is ready:', ipfs)
     const dir = await ipfs.addFromFs('./test-content/', { recursive: true, hidden: true })
 
     console.log(dir)
+    
+    Dat('./.hash-storage/', (err, dat) => {
+        if (err) throw err
+
+        console.log('dat is ready:', dat)
+    })
 })
