318d0f4a6c128e6d4aa18afa116011ec71cf1f6f
Jacob Karlsson <jacob.karlsson95@gmail.com>
2019-05-09 23:59:11 +0200

    Standard fix

--- index.js
+++ index.js
@@ -2,26 +2,26 @@ const fs = require('fs')
 const IPFS = require('ipfs')
 const Dat = require('dat')
 
-//exports.init = functi
+// exports.init = functi
 const ipfs = new IPFS()
 
 ipfs.on('ready', async () => {
-    const dir = await ipfs.addFromFs('./test-content/', { recursive: true, hidden: true })
+  const dir = await ipfs.addFromFs('./test-content/', { recursive: true, hidden: true })
 
-    console.log(dir)
-    
-    const dirHash = dir[dir.length - 1].hash
+  console.log(dir)
 
-    fs.writeFile("./.hash-storage/ipfs-hash.txt", dirHash, err => {
-        if (err) throw err
+  const dirHash = dir[dir.length - 1].hash
 
-        Dat('./.hash-storage/', (err, dat) => {
-            if (err) throw err
+  fs.writeFile('./.hash-storage/ipfs-hash.txt', dirHash, err => {
+    if (err) throw err
 
-            dat.importFiles({watch: true})
-            dat.joinNetwork()
+    Dat('./.hash-storage/', (err, dat) => {
+      if (err) throw err
 
-            console.log('My Dat link is: dat://' + dat.key.toString('hex'))
-        })
+      dat.importFiles({ watch: true })
+      dat.joinNetwork()
+
+      console.log('My Dat link is: dat://' + dat.key.toString('hex'))
     })
+  })
 })
